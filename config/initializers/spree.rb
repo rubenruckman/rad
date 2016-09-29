1f312b2fd4e4fffe878eadc9fea713fd45d11c97
Maxim Pechnikov <parallel588@gmail.com>
2016-09-29 08:14:38 +0300

    disable s3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -20,7 +20,7 @@ end
 
 # Paperclip::Attachment.default_options.merge!(
 #     YOUR OPTIONS FOR S3 HERE
-# ) 
+# )
 
 #paperclip_defaults = {
 #
@@ -39,35 +39,35 @@ end
 #  path: "/:class/:id/:style/:clean_filename"
 #}
 
-attachment_config = {
+# attachment_config = {
 
-  s3_credentials: {
-    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    bucket:            ENV['S3_BUCKET_NAME']
-  },
+#   s3_credentials: {
+#     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+#     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#     bucket:            ENV['S3_BUCKET_NAME']
+#   },
 
-  storage:        :s3,
-  s3_headers:     { "Cache-Control" => "max-age=31557600" },
-  s3_protocol:    "https",
-  bucket:         ENV['S3_BUCKET_NAME'],
-  url:            ":s3_domain_url",
+#   storage:        :s3,
+#   s3_headers:     { "Cache-Control" => "max-age=31557600" },
+#   s3_protocol:    "https",
+#   bucket:         ENV['S3_BUCKET_NAME'],
+#   url:            ":s3_domain_url",
 
-  styles: {
-      mini:     "48x48>",
-      small:    "100x100>",
-      product:  "240x240>",
-      large:    "600x600>"
-  },
+#   styles: {
+#       mini:     "48x48>",
+#       small:    "100x100>",
+#       product:  "240x240>",
+#       large:    "600x600>"
+#   },
 
-  path:           "/:class/:id/:style/:basename.:extension",
-  default_url:    "/:class/:id/:style/:basename.:extension",
-  default_style:  "product"
-}
+#   path:           "/:class/:id/:style/:basename.:extension",
+#   default_url:    "/:class/:id/:style/:basename.:extension",
+#   default_style:  "product"
+# }
 
-attachment_config.each do |key, value|
-  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-end
+# attachment_config.each do |key, value|
+#   Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+# end
 
 #paperclip_defaults.each do |key, value|
 #  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
