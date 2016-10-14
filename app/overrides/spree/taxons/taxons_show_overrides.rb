d2081ff603832ded3d3eb3757a242fbbf3347538
Maxim Pechnikov <parallel588@gmail.com>
2016-10-14 08:26:05 +0300

    fix css

--- app/overrides/spree/taxons/taxons_show_overrides.rb
+++ app/overrides/spree/taxons/taxons_show_overrides.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_header",
-                     :insert_before => '#logo',
-                     :text          => "
+Deface::Override.new(virtual_path: "spree/shared/_header",
+                     insert_before: '#logo',
+                     text: "
 <% if @taxon && @taxon.image.present? %>
 <div id='taxon-image-collection'>
    <div class='bcg skrollable skrollable-between' style='background-image: url(\"<%= @taxon.image.url(:large).html_safe %>\");' >
@@ -16,9 +16,9 @@ Deface::Override.new(:virtual_path  => "spree/shared/_header",
 
 
 
-Deface::Override.new(:virtual_path  => "spree/shared/_main_nav_bar",
-                     :insert_after => '#main-nav-bar',
-                     :text          => "
+Deface::Override.new(virtual_path: "spree/shared/_main_nav_bar",
+                     insert_after: '#main-nav-bar',
+                     text: "
 <% if @taxon && @taxon.image.present? %>
 <div id='mob-taxon-image-collection' style='display:none;'>
    <div class='bcg skrollable skrollable-between' style='background-image: url(\"<%= @taxon.image.url(:large).html_safe %>\");' >
@@ -32,18 +32,23 @@ Deface::Override.new(:virtual_path  => "spree/shared/_main_nav_bar",
                               ",
                      name: "mob-taxon_image_page")
 
-Deface::Override.new(:virtual_path  => "spree/taxons/show",
-                     :insert_before => '[data-hook="taxon_products"]',
-                     :text          => "
+Deface::Override.new(virtual_path: "spree/taxons/show",
+                     insert_before: '[data-hook="taxon_products"]',
+                     text: "
 <% if @taxon && @taxon.page.present? %>
 <%= @taxon.page.body.to_s.html_safe %>
 <% end %>
                               ",
                      name: "taxon_image_page")
 
-Deface::Override.new(:virtual_path  => "spree/shared/_nav_bar",
-                     :insert_after => '.navbar-toggle',
-                     :text          => "
-yyyyyyyyyyyyy
-",
-                     name: "taxon_imagffffffffe_page")
+Deface::Override.new(virtual_path: "spree/taxons/show",
+                     replace: '.taxon-title',
+                     text: "
+<div>
+<div class='page_content_10' style='margin-bottom: 9px;'>
+<div class='row' style='margin-left: -21px;'>
+<h1 class='taxon-title'><%= @taxon.name %></h1>
+</div></div>
+</div>
+                              ",
+                     name: "taxon_page_title")
